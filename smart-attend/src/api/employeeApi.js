d0818a92366597eef0381637b451bb1f4877053a
jangidnitin2006-Engineer <jangidnitin2006@gmail.com>
2026-07-06 10:41:57 +0530

    Updated API URL for production

--- smart-attend/src/api/employeeApi.js
+++ smart-attend/src/api/employeeApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smartattend-1-0a3x.onrender.com/api",
 });
 
 // Automatically attach JWT to every request
